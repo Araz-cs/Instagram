e1186b7e1ea08223aab95b803fe4e8a1c42543e4
Araz-cs <araz.sultanian@gmail.com>
2020-02-22 20:28:54 -0800

    2nd instagram updated

---
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -0,0 +1,36 @@
+package com.example.instagram.fragments;
+
+import android.util.Log;
+
+import com.example.instagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+    @Override
+    protected void queryPosts() {
+        super.queryPosts();
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+        postQuery.include(Post.KEY_USER);
+        postQuery.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        postQuery.setLimit(20);
+        postQuery.addDescendingOrder(Post.KEY_CREATED_KEY);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "error with query");
+                    return;
+                }
+                for (int i = 0; i < posts.size(); i++)
+                    Log.d(TAG, "Post: " + posts.get(i).getDescription() + " username: " + posts.get(i).getUser().getUsername());
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
